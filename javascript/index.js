4464e80ab436f922179438a7caecb23a6f9fc039
jair_orozco <dg.jairsanmartin@gmail.com>
2020-12-14 20:01:42 -0600

    Se creó la carpeta plugins; el primero es el de AutoPlay. Para que funcionara el plugin era necesario quitar el audio por lineamientos de HTML. Para devolver el audio al reproductor se agregó la función y se coloco en el botón correspondiente en el reproductor.

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,10 @@
 import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from '../plugins/AutoPlay.js'
 
 const video = document.getElementById("movie");
 const button = document.getElementById("play-pause");
-const player = new MediaPlayer({el: video});
+const player = new MediaPlayer({el: video, plugins: [new AutoPlay()]});
+const button_sound = document.getElementById("unmute");
 
 button.onclick = () => player.togglePlay();
+button_sound.onclick = () => player.unmute();
