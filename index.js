d6a2a2bef3bca962cee6a78eda466026f7d88979
jair_orozco <dg.jairsanmartin@gmail.com>
2020-12-14 18:30:16 -0600

    Con javascript se dio la funcionalidad del reproductor para dar play y pause.

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const video = document.querySelector("video");
-const button = document.querySelector("button");
+const video = document.getElementById("movie");
+const button = document.getElementById("play-pause");
+const player = new MediaPlayer({el: video});
 
-button.onclick = () => {
-    video.play();
+
+function MediaPlayer (config) {
+    this.media = config.el;
 }
+
+MediaPlayer.prototype.play = function() {
+        this.media.play();
+}
+
+MediaPlayer.prototype.pause = function() {
+    this.media.pause();
+}
+
+MediaPlayer.prototype.togglePlay = function() {
+    if (this.media.paused){
+        this.play();
+    }else {
+        this.pause();
+    }
+}
+
+button.onclick = () => player.togglePlay();
